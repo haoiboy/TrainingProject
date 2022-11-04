1db1448bf0a00bbfd40f5ee5dcd0c06c0b60179b
靓仔阿波\波波 <1308776338@qq.com>
2022-11-04 21:08:16 +0800

    new

--- mybatis_helloworld/src/main/java/com/atguigu/mybatis/pojo/mapper/UserMapper.java
+++ mybatis_helloworld/src/main/java/com/atguigu/mybatis/pojo/mapper/UserMapper.java
@@ -1,6 +1,34 @@
 package com.atguigu.mybatis.pojo.mapper;
 
+import com.atguigu.mybatis.pojo.User;
+
+import javax.swing.*;
+import java.util.List;
+
 public interface UserMapper {
+    /**
+     * 添加用户信息
+     * @return
+     */
     int insertUser();
 
+    /**
+     * 修改用户信息
+     */
+    void updateUser();
+
+    /**
+     * 删除用户信息
+     */
+    void deleteUser();
+
+    /**
+     * 根据ID查询用户信息
+     * @return
+     */
+    User getUserById();
+    /**
+     *查询所有的用户信息
+     */
+     List<User> getAllUser();
 }
