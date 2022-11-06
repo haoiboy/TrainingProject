13b410627319b646d8d74b3441a637daaa89d9d6
靓仔阿波\波波 <1308776338@qq.com>
2022-11-06 17:10:02 +0800

    new

--- mybatis_parameter/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
+++ mybatis_parameter/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
@@ -2,6 +2,19 @@ package com.atguigu.mybatis.mapper;
 
 import com.atguigu.mybatis.pojo.User;
 
+import java.util.Map;
+
+/**
+ * mybatis获取参数值的两种方式：#{}和${}
+ * #{}的本质是占位符赋值，${}的本质是字符串拼接
+ * 1.若mapper接口方法的参数为单个的字面量类型
+ * 此时可以通过#{}和${}任意内容获取参数值，一定要注意${}的单引号问题
+ * 2.若mapper接口方法的参数为多个的字面量类型
+ * 此时Mybatis会将参数放在map集合里，以两种方式存储数据
+ * a>以arg0,arg1..为键,以参数值为值
+ * b>以param1，param2..为键，以参数为值
+ * 因此，只需要#{}和${}访问map集合的键，就可以获取对应的值
+ */
 public interface UserMapper {
     /**
      * 根据用户名查询用户信息
@@ -9,4 +22,19 @@ public interface UserMapper {
      * @return
      */
     User getUserByUsername(String username);
+
+    /**
+     * 验证登录
+     * @param username
+     * @param password
+     * @return
+     */
+    User checkLogin(String username,String password);
+
+    /**
+     * 验证登录（以map集合作为参数）
+     * @param map
+     * @return
+     */
+    User checkLoginByMap(Map<String,Object>map);
 }
--- mybatis_parameter/src/test/java/com/atguigu/mybatis/test/ParameterTest.java
+++ mybatis_parameter/src/test/java/com/atguigu/mybatis/test/ParameterTest.java
@@ -6,6 +6,10 @@ import com.atguigu.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParameterTest {
     @Test
     public void testGetUserByUsername(){
@@ -13,5 +17,24 @@ public class ParameterTest {
         UserMapper mapper=sqlSession.getMapper(UserMapper.class);
         User user=mapper.getUserByUsername("admin");
         System.out.println(user);
+    }
+    @Test
+    public void testcheckLogin(){
+        SqlSession sqlSession= SqlSessionUtil.getSqlsession();
+        UserMapper mapper=sqlSession.getMapper(UserMapper.class);
+       User  user=mapper.checkLogin("admin","123456");
+        System.out.println(user);
+
+    }
+    @Test
+    public void testcheckLoginByMap(){
+        SqlSession sqlSession= SqlSessionUtil.getSqlsession();
+        UserMapper mapper=sqlSession.getMapper(UserMapper.class);
+        Map<String,Object>map=new HashMap<>();
+        map.put("username","admin");
+        map.put("passwprd","123456");
+        User  user=mapper.checkLoginByMap(map);
+        System.out.println(user);
+
     }
 }
