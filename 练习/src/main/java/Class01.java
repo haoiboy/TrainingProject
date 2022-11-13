33608b2d92c1ec0f8fea0c8fe9a5ace50fb5e5ec
靓仔阿波\波波 <1308776338@qq.com>
2022-11-14 00:20:04 +0800

    new

---
+++ 练习/src/main/java/Class01.java
@@ -0,0 +1,19 @@
+public class Class01 {
+//     public  static  void  main(String[] args){
+//           int sum=0;
+//           for(int i=0;i<5;i++){
+//               sum+=i;
+//           }
+//         System.out.println(sum);
+//
+//     }
+//
+
+    public static void main(String[] args) {
+int [] arr=new int[3];
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+
+    }
+}
