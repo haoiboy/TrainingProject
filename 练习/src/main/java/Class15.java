8bf5fea832bd0e04426c7a8a522b1eb360fc6eea
靓仔阿波\波波 <1308776338@qq.com>
2022-11-15 22:44:32 +0800

    debug调试程序              栈帧链 压栈出栈 先进后出

---
+++ 练习/src/main/java/Class15.java
@@ -0,0 +1,17 @@
+public class Class15 {
+    public static void main(String[] args) {
+        /**
+         * 小游戏：逢7报数
+         * 1.数据在1~100之间，用for循环实现数据的获取
+         * 2.根据规则，用if语句实现数据的判断，要么个位是7，要么十位是7，要么能被7整除
+         * 解析：x%10==7   x/10%10==7     x%7==0
+         * 3.在控制台输出满足规则的数据
+         */
+        System.out.println("逢7过");
+        for(int x=1;x<100;x++){
+            if(x%10==7||x/10%10==7||x%7==0){
+                System.out.println(x);
+            }
+        }
+    }
+}
---
+++ 练习/src/main/java/Class19.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Class19 {
+    /**
+     * 根据用户输入的一个数据，查找该数据在数组中对应的位置
+     * @param userData 用户输入的数据
+     * @param arr 需要查找的数组
+     * @return 在数组中 对应的index位置
+     */
+    public static void main(String[] args) {
+        //1.定义一个数组
+        int[] arr={29,68,33,44,10};
+        System.out.println("用户请您输入数据：");
+        //2.键盘录入数据
+        Scanner scanner=new Scanner(System.in);
+        //3.获取到用户输入的数据
+        int userData = scanner.nextInt();
+        int index= getIndex(userData,arr);
+        //三元表达式
+        System.out.println(index==-1?"很抱歉，没有找到该数据， 所以返回-1"
+                :"查找到该数据，对应的index位置为 "+index);
+    }
+    public  static int getIndex(int userData ,int[] arr){
+        //4.int index=-1
+        int index=-1;
+        //5.循环遍历整个数组
+        for (int i=0;i<arr.length;i++){
+            if(userData==arr[i]){
+               return i;
+            }
+        }
+
+        return  -1;
+    }
+
+}
