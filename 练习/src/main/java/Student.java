326c07c3badbf2772cadd46a5f36791c3af944b3
靓仔阿波\波波 <1308776338@qq.com>
2022-11-17 17:28:59 +0800

    面向对象 对象的使用

---
+++ 练习/src/main/java/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    //学生类中 成员属性
+    String name;
+    int age;
+    /**
+     * 成员方法
+     */
+    public void work(){
+        System.out.println("开始使用键盘敲代码....");
+    }
+}
